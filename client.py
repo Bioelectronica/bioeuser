3831d8a77241f2bf2397790285334f359f63f88a
a <a>
2020-12-18 19:38:08 -0800

    client script

---
+++ client.py
@@ -0,0 +1,33 @@
+import zmq
+import sys
+import json
+
+if len(sys.argv) < 2:
+    print("No arguments given!")
+    sys.exit()
+elif len(sys.argv) < 3:
+    ia = list(sys.argv[-1])
+else:
+    ia = list(sys.argv[1:])
+
+c1 = zmq.Context()
+s1 = c1.socket(zmq.REQ)
+s1.connect("tcp://master:5551")
+
+c2 = zmq.Context()
+s2 = c2.socket(zmq.REQ)
+s2.connect("tcp://slave:5552")
+
+for x in [ia]:
+    if sys.argv[1]=='JSON':
+        with open(ia[2],'r') as f:
+            js=json.load(f)
+        ia=[ia[0],ia[1],js]
+    s1.send_pyobj(ia)
+    s2.send_pyobj(ia)
+    print("Sending...")
+    msg=s1.recv_string()
+    msg2=s2.recv_string()
+    print(msg)
+    print(msg2)
+    

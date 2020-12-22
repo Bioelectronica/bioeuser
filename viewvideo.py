29b2d0fa87cfd0fb6a019086f6fb8d217bbbde03
cn1 <a>
2020-12-21 23:18:01 -0800

    ran for 5000 frames with an error

--- viewvideo.py
+++ viewvideo.py
@@ -5,7 +5,7 @@ from multiprocessing import RawArray
 import pdb
 import time
 import os
-
+import logging
 
 
 #arr=RawArray('d',640*3*480*2)
@@ -15,6 +15,8 @@ def connect_to_streams(zmqport):
     socket = context.socket(zmq.SUB)
     socket.bind("tcp://{}:{}".format("*",str(zmqport)) )
     socket.subscribe("")
+    #poller=zmq.Poller()
+    #poller.register(socket,zmq.POLLIN)
     #socket.setsockopt(zmq.CONFLATE,1)
     return (socket,context)
 
@@ -25,20 +27,28 @@ def create_windows(p,i,n):
     cv2.resizeWindow(camstr, 640, 480)
     cv2.moveWindow(camstr, 100+(i*20), 100)
 
-
-
-
-
-def viewdata(camstr,socket):
-    dat=socket.recv_pyobj()
+def getdata(camstr,socket):
     try:
-        if len(dat)>0 :
-            cv2.imshow(camstr,dat)
-        else:
-            return True
-    except TypeError:
-        t=1
-    if cv2.waitKey(1) & 0xFF==ord('q'):
+        logging.info(camstr)
+        dat=socket.recv_pyobj(flags=zmq.NOBLOCK)
+        fname='/tmp/'+camstr+'.jpg'
+        cv2.imwrite(fname,dat,[int(cv2.IMWRITE_JPEG_QUALITY),90])
+        logging.info(camstr+'done with img')
+        return True
+    except zmq.error.Again as ef:
+        return False
+    except Exception as e:
+        logging.exception("Exception caught")
+        return False
+
+def viewdata(camstr):
+    try:
+        im=cv2.imread('/tmp/'+camstr+'.jpg')
+        #pdb.set_trace()
+        cv2.imshow(camstr,im)
+    except Exception as e:
+        logging.exception("Exception caught")
+    if cv2.waitKey(10) & 0xFF==ord('q'):
         return False
     return True
 
@@ -52,8 +62,16 @@ names = ['merge1',\
         'sample8']
 ports=["7000","7010","7020","7030","8000","8010","8020","8030"]
 
+logging.basicConfig(format='%(asctime)s %(threadName)s %(message)s',\
+filename='/home/bioeuser1/logs/vidlog.log',\
+level=logging.DEBUG)
+
 time.sleep(30)
 pclist=[]
+imbool=[]
+imlist=[]
+
+
 for i in ports:
     pclist.append(connect_to_streams(i))
 
@@ -62,16 +80,23 @@ for i,x in enumerate(ports):
 
 plist=[x[0] for x in pclist]
 clist=[x[1] for x in pclist]
-
-
+#polist=[x[2] for x in pclist]
 
 gjobs=[]
 
 b=True
 b2=True
 while(os.path.isfile('/home/bioeuser1/expdone')==False):
+    gjobs=[]
+    for idx,camOI in enumerate(names):
+        g=threading.Thread(target=getdata,name=camOI,args=(camOI,plist[idx],))
+        gjobs.append(g)
+    for k in gjobs:
+        k.start()
+    for k in gjobs:
+        k.join()
     for i,p in enumerate(ports):
-        b=viewdata(names[i],plist[i])
+        b=viewdata(names[i])
         if b==False:
             b2=False
             break

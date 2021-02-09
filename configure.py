f175ef4aa24c8f68efd8116a9fb0431c723ef04d
Amar Basu <amar@bioelectronica.com>
2021-02-08 21:34:26 -0800

    cleaned up code

--- configure.py
+++ configure.py
@@ -4,17 +4,65 @@ import subprocess as sp
 
 
 
-""" This file contains code to update the configuration of the Hypercell system 
+""" This file contains functions to update the configuration of the Hypercell system
+
+TODO 
+Things to test:
+- can't find files in directory - should throw exception
+- newsettings does not match existing settings
+- newsettings defines empty 'particlecriteria' key
+- no connection to slave
+
 """
 
+#
 # Constants
+#
+
+# directory on master and slave NUCS where settings are found
 data_dir = '/data/default_settings'
+
+# list of hypercell cameras which map to folder names in data directories
 cameras = ['merge1', 'merge2', 'merge5', 'merge6',
     'sample4', 'sample8', 'waste3', 'waste7']
+
+# list of settings jsons, eg rta_settings_merge1.json, rta_settings_merge2.json, ..
 settings_json = [data_dir + '/rta_settings_' + c + '.json' 
         for c in cameras]
 slave_url = 'slave'
- 
+
+#
+# Main functions
+#
+
+def update_hypercell_cfg(newsettings):
+    """ Updates hypercell default settings in both master and slave nuc
+    This function is intended to be run on the master nuc, and changes
+    are copied over to the slave NUC
+
+    Example:
+        newsettings = {'particlecriteria': {
+                        'Radius': [12, None],
+                        'Differential Grayscale Mean': [65, None]}
+                        }
+        update_hypercell_cfg(newsettings)
+
+    Args:
+        newsettings(dict): settings to change.  Only keys in this dict will be updated
+    """
+    global settings_json
+
+    # Update the merge, sample, waste jsons with the user selected value
+    # (Reminder: this is done on the master NUC)
+    [update_json_nested(f, newsettings) for f in settings_json]
+
+    copy_cfg_to_slave()
+
+
+#
+# Helper functions
+#
+
 def update_dict(d1, d2):
     """Update dictionary d1 with values in dictionary d2.
     This is similar to the python dict update function, except it updates
@@ -45,6 +93,7 @@ def update_dict(d1, d2):
         else:
             d1[k] = v
 
+
 def update_json_nested(path, d):
     """Update the json file contained in path with settings in dict d
         Only the keys listed in dict will be updated
@@ -59,45 +108,8 @@ def update_json_nested(path, d):
         json.dump(js,nf,indent=4)
 
 
-def update_json(path,d,jl):
-    """ NOT USED CURRENTLY 
-    Updates a json file with a new dict
-
-    Args:
-        path(str): name of json file to update
-        d(dict): dictionary with updated keys
-        jl(str): if provided, update only the given key
-
-    """
-    with open(path,'r') as f:
-        js=json.load(f)
-    for k in d.keys():
-        if jl=='':
-            js.update({k:d[k]})
-        else:
-            js[jl].update({k:d[k]})
-    with open(path,'w') as nf:
-        json.dump(js,nf,indent=4)
-
-
-def update_hypercell_cfg(newsettings):
-    """ Updates hypercell default settings in both master and slave nuc
-    This function is intended to be run on the master nuc, and changes
-    are copied over to the slave NUC
-
-    Args:
-        newsettings(dict): updated newsettings.  Only keys in this will be updated
-    """
-    global settings_json
-
-    # Update the merge, sample, waste jsons with the user selected value
-    # (Reminder: this is done on the master NUC)
-    [update_json_nested(f, newsettings) for f in settings_json]
-
-    copy_cfg_to_slave()
-
 def copy_cfg_to_slave():
     """ Copies all configuration files to slave """
     # Now copy these file to the slave, so it has the same settings
-    command ="scp {} slave:{}".format(' '.join(settings_json), data_dir) 
+    command ="scp {} slave:{}".format(' '.join(settings_json), data_dir)
     sp.run([command], shell=True)
